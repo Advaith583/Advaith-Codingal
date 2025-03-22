a290c01a49d69620ce91d682153f407fa37aa269
Advaith583 <advaithharith10@gmail.com>
2025-03-22 09:59:44 +0200

    ACP

---
+++ Java Script/Lesson 4/Functions.js
@@ -0,0 +1,17 @@
+let book = {
+    title: "Java script for beginers",
+    Author: "Jhon Smith",
+    Price: "$29.99",
+};
+console.log("Book Title:",book.title);
+console.log("Author:",book.Author);
+console.log("Book Price:",book.Price);
+
+function printBookDetails(obj){
+    console.log("\nBook Details:");
+    console.log("Title:",obj.title);
+    
+    console.log("Author:",obj.Author);
+    console.log("Price:",obj.Price);
+
+}
