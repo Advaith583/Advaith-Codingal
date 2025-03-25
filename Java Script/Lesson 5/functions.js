92f395debcce75b3fb9127b93d3bf30effb69556
Advaith583 <advaithharith10@gmail.com>
2025-03-25 18:55:19 +0200

    ACP

---
+++ Java Script/Lesson 5/functions.js
@@ -0,0 +1,16 @@
+function first(){
+    console.log("hello first")
+}
+function second(){
+    first()
+    console.log("hello second")
+}
+function third(){
+    second()
+    console.log("hello third")
+}
+function fourth(){
+    third()
+    console.log("Hello Advaith")
+}
+fourth()
